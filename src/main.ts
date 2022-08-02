21b7d36d2eb1495065578fa16b54aae1ed338a78
Navfront <b3rt@yandex.ru>
2022-08-03 00:30:18 +0300

    port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function start() {
-  const PORT = process.env.PORT || 5500;
+  const PORT = process.env.PORT || 8080;
 
   try {
     const app = await NestFactory.create(AppModule);
