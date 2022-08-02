2cf39cbb5fac2c4b37bb5d823d1e78c02dcef96f
Navfront <b3rt@yandex.ru>
2022-08-02 20:28:08 +0300

    refactoring

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 
-@ApiTags('Авторизация')
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Авторизация' })
   @Post('/login')
   login(@Body() dto: CreateUserDto) {
     return this.authService.login(dto);
   }
 
+  @ApiOperation({ summary: 'Регистрация' })
   @Post('/registration')
   registration(@Body() dto: CreateUserDto) {
     return this.authService.registration(dto);
