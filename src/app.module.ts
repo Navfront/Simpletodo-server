e0fb91d4533de10c6adbde14f770c9bd901989d3
Navfront <b3rt@yandex.ru>
2022-08-02 18:32:25 +0300

    install jwt bcrypt and add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [ConfigModule.forRoot({ envFilePath: '.env' }), UsersModule, TodosModule],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env' }), UsersModule, TodosModule, AuthModule],
 })
 export class AppModule {}
